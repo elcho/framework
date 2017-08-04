6b75de9836f066246aae261d42fa362e693e9f87
Elchonon <elchononspiegel@gmail.com>
2017-08-04 00:06:33 -0400

    created the Symfony HttpFoundation component

--- index.php
+++ index.php
@@ -1,6 +1,51 @@
 <?php
-$input = isset($_GET['name']) ? $_GET['name'] : 'World';
+require_once __DIR__.'/vendor/autoload.php';
 
-header('Content-Type: text/html; charset=utf-8');
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-printf('Hello %s', $input);
+$request = Request::createFromGlobals();
+
+//can now access the full API with the Request class:
+// the URI being requested (e.g. /about) minus any query parameters
+$request->getPathInfo();
+
+// retrieve GET and POST variables respectively
+$request->query->get('foo');
+$request->request->get('bar', 'default value if bar does not exist');
+
+// retrieve SERVER variables
+$request->server->get('HTTP_HOST');
+
+// retrieves an instance of UploadedFile identified by foo
+$request->files->get('foo');
+
+// retrieve a COOKIE value
+$request->cookies->get('PHPSESSID');
+
+// retrieve an HTTP request header, with normalized, lowercase keys
+$request->headers->get('host');
+$request->headers->get('content_type');
+
+$request->getMethod();    // GET, POST, PUT, DELETE, HEAD
+$request->getLanguages(); // an array of languages the client accepts
+
+if ($myIp === $request->getClientIp(true)) {
+    // the client is a known one, so give it some more privilege
+}
+
+//simulate a request:
+$input = $request->get('name', 'World');
+
+//can now access the full API with the Response class:
+$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+
+//or change around the response:
+$response->setContent('Hello world!');
+$response->setStatusCode(200);
+$response->headers->set('Content-Type', 'text/html');
+
+// configure the HTTP cache headers
+$response->setMaxAge(10);
+
+$response->send();
