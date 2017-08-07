b575dcbb7c839c0c113c266cb70eef1839feb418
Elchonon <elchononspiegel@gmail.com>
2017-08-06 22:56:22 -0400

    templated routes

--- src/app.php
+++ src/app.php
@@ -1,8 +1,52 @@
 <?php
 use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Response;
 
 $routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'World')));
-$routes->add('bye', new Routing\Route('/bye/{name}'));
+
+$routes->add('hello', new Routing\Route('/hello/{name}', array(
+    'name' => 'World',
+    '_controller' => 'render_template',
+)));
+
+$routes->add('bye', new Routing\Route('/bye/{name}', array(
+    'name' => 'World',
+    '_controller' => 'render_template',
+)));
+
+function is_leap_year($year = null) {
+    if (null === $year) {
+        $year = date('Y');
+    }
+
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
+    'year' => null,
+    '_controller' => function ($request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yep, this is a leap year!');
+        }
+
+        return new Response('Nope, this is not a leap year.');
+    }
+)));
+
+function my_age_in_2050($age = null) {
+    if (null === $age) {
+        $age = 0;
+    }
+    $year_now = date('Y');
+    return (2050 - $year_now) + $age;
+}
+
+$routes->add('my_age_in_2050', new Routing\Route('/my_age_in_2050/{age}', array(
+    'age' => null,
+    '_controller' => function ($request) {
+        
+        return new Response(my_age_in_2050($request->attributes->get('age')));
+    }
+)));
 
 return $routes;
